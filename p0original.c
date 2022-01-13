45a331d067e9a058824dfdd181d791fed93a2b18
Hmm <97089360+hmmm444@users.noreply.github.com>
2022-01-13 15:54:53 +0000

    0,1

--- p0original.c
+++ p0original.c
@@ -1,1 +1,6 @@
- 
+#include<stdio.h>
+int main()
+{
+  printf("My name is Manovikyath");
+  return 0;
+}  
--- p1original.c
+++ p1original.c
@@ -1,2 +1,11 @@
-
-}
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  float sum;
+  printf("enter two number\n");
+  scanf("%d%d",&num1,&num2);
+  sum = num1 + num2;
+  printf("Sum of %d + %d = %f \n",num1,num2,sum);
+  return 0;
+} 
